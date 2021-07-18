37fbf81c77b342b847c211fdb5bc8db205d65841
Misak Melikyan <misak@simplepractice.com>
2021-07-18 16:57:57 -0700

    Add pagination support

--- list.js
+++ list.js
@@ -2,8 +2,11 @@ const axios = require("axios");
 const JSONAPIDeserializer = require("jsonapi-serializer").Deserializer;
 
 exports.handler = async(event) => {
+  console.log("EVENT DUMP:", event);
+  const page = event.query.page || 1;
+  const count = event.query.count || 10;
   const baseUri = "https://qa02.monarch-staging.com";
-  const cliniciansSerialized = await axios.get(`${baseUri}/api/clinicians`, {
+  const cliniciansSerialized = await axios.get(`${baseUri}/api/clinicians?page[number]=${page}&page[size]=${count}`, {
     headers: {
       Authorization: "Basic YmV0YTpoZWFsdGgxMw=="
     }
